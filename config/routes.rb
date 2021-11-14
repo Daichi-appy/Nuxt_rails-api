54617c5fba34e3020e88ad34f649ec80ef1cbd5f
daichi.aoki <daichi.es47@gmail.com>
2021-11-14 11:56:30 +0900

    add_user_token_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      # users
       resources :users, only:[] do
         get :current_user, action: :show, on: :collection
       end
+
+      # login, logout
+      resources :user_token, only: [:create] do
+        delete :destroy, on: :colloection
+      end
+      
     end
   end
 end
